bda7c78d4207e24c28b012ae0c43c4199c8787fc
Benjamin Cheng <bencheng200@gmail.com>
2021-05-23 18:03:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
 }
 
 function deleteFromDriverByKey(driver, key) {
+  
+  const newObj = Object.assign({}, driver)
+  
   delete driver[key]
   
-  
+  return newObj
 }
